89a310aa76610695ef44deeb09d531819a7e411c
seorimpark <ssseeeooo000@gmail.com>
2023-05-24 12:29:18 +0200

    checkpoint

--- src/framework.py
+++ src/framework.py
@@ -15,7 +15,11 @@ output:
 # from sequence_preprocessing import *
 import Bio
 from Bio import SeqIO
+from Bio import AlignIO
+from Bio.Align import MultipleSeqAlignment
+from Bio.Align.AlignInfo import SummaryInfo
 from sequence_preprocessing import *
+from itertools import chain
 
 
 def load_fasta(filename):
@@ -27,6 +31,7 @@ def load_fasta(filename):
 class ConsensusSeq:
     def __init__(self, name, filename=None, seq=None):
         self.name = name  # name of the algorithm
+        self.filename = None
         if filename:
             self.filename = filename
             self.sequence = read_fastq(filename)
@@ -39,6 +44,16 @@ class ConsensusSeq:
         self.evalue = 0
         self.coverage = 0  # percentage of coverage
 
+        # Example of consensus? https://stackoverflow.com/questions/73702044/how-to-get-a-consensus-of-multiple-sequence-alignments-using-biopython
+
+    def get_consensus_seq(self):
+        common_alignment = MultipleSeqAlignment(
+            chain(*AlignIO.parse(self.filename, "fastq"))
+        )
+        summary = SummaryInfo(common_alignment)
+        consensus = summary.dumb_consensus(0.7, "N")
+        self.sequence = consensus
+
     def align_ref(self, ref):
         aligner = Bio.Align.PairwiseAligner()
         alignments = aligner.align(self.sequence, ref)
@@ -48,8 +63,8 @@ class ConsensusSeq:
         print("Coverage: " + str(self.coverage))
 
 
-# MultiSeq = MultiSequence("rbcL_MN_tomato.pickle")
 ref = load_fasta("rbcL_NCBI_tomato.fasta")
-output = ConsensusSeq("test", "rbcL_NCBI_tomato.fasta")
+output = ConsensusSeq("test", "BestSequences_rbcL_Qiagen_tomato_5000.fastq")
 # print(MultiSeq.consensus)
+output.get_consensus_seq()
 output.align_ref(ref)
