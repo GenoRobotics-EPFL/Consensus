feabb175df0d5c4a75fc83894221c1d30c915a19
Ghassan Abboud <ghassan.abboud@epfl.ch>
2024-03-24 18:15:13 +0100

    implement command line

--- fastq_gen/utils.py
+++ fastq_gen/utils.py
@@ -1,7 +1,9 @@
 import random
 from Bio.Seq import Seq
 import numpy as np
+from Bio import Entrez
 
+Entrez.email = "ghassan.abboud@epfl.ch"
 def generate_weighted_sequence_variant(sequence, weights=[0.25, 0.25, 0.25, 0.25]):
     """
     flips an initial sequence as it could happen when using the MinIon tool
@@ -105,3 +107,46 @@ def assign_quality_scores(sequence, mutation_probability=0.1,mutation_mean = 16,
         base64_score = base64_table[int(round(quality_score))]
         quality_scores.append(base64_score)
     return ''.join(quality_scores)
+
+def download_sequence(species, gene_name, dst, start_length=None, stop_length= None, id = None, permissive_search = True):
+    """
+    download sequence from GenBank through the Entrez database. 
+
+    Parameters:
+    ----------
+    species(str): name of species
+    gene_name(str): name of gene
+    dst(str,Path-like): destination file path
+    start_length(int): minimum length of sequence
+    stop_length(int): maximum length of sequence
+    id(list): list of NCBi ids of sequences to download. If provided, overrides gene_name and species.
+    permissive_search(bool, default = True): when True, if Advanced NCBI query returns nothing, replace it with a less precise general query.
+    """
+    
+    if id == None:
+        search_term = f"{gene_name}[Gene Name] AND {species}[Organism]"
+        print(search_term)
+        if start_length!= None or stop_length!= None:
+                search_term += f" {start_length}:{stop_length}[Sequence Length]"
+        handle = Entrez.esearch(db = "nucleotide", term = search_term, retmax = 1, idtype = "acc")
+        search_result = Entrez.read(handle)
+        handle.close()
+        id = search_result["IdList"]
+        n=0
+        for i in id:
+            n+=1
+        if n==0 and permissive_search:
+            search_term = f"{gene_name} {species} {start_length}:{stop_length}[Sequence Length]"
+            handle = Entrez.esearch(db = "nucleotide", term = search_term, retmax = 1, idtype = "acc")
+            search_result = Entrez.read(handle)
+            handle.close()
+            id = search_result["IdList"]
+    n=0
+    for i in id:
+        n+=1
+    if n==1:
+        handle = Entrez.efetch(db="nucleotide", id=id, retmode = "fasta", rettype = "fasta")
+        sequence = handle.read()
+        handle.close()
+        with open(dst, mode="a") as writer:
+            writer.write(sequence)
